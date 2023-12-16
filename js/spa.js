c78c0c1834388f99961146c766d85fd1d9b68a89
sarabasane <saraba.sane@sfr.fr>
2023-12-16 20:36:47 +0100

    ajout panier.html, panier.js, spa.js, reservation.js

---
+++ js/spa.js
@@ -0,0 +1,37 @@
+$('.jqFuwattoAsClassL').fuwatto({
+    duration: 1000
+  })
+
+    $('.jqFuwattoAsClassL').fuwatto({
+  slide: 'left-right'
+})
+
+    $('.jqFuwattoAsClassL').fuwatto({
+    distance: 2000,
+  })
+
+    $('.jqFuwattoAsClassL').fuwatto({
+    adjustment: {
+      top: 0,
+      left: 0,
+    }
+  })
+
+    $('.jqFuwattoAsClassR').fuwatto({
+    duration: 2000
+  })
+
+    $('.jqFuwattoAsClassR').fuwatto({
+    slide: 'right-left'
+  })
+
+    $('.jqFuwattoAsClassR').fuwatto({
+    distance: 2000,
+  })
+
+    $('.jqFuwattoAsClassR').fuwatto({
+    adjustment: {
+      top: 0,
+      left: 0,
+    }
+})
